71ae66e71f08d6c16b00ba1ebc194de70bb272ea
Iryna <ivanovska.iryna@gmail.com>
2017-03-22 14:30:14 +0100

    Update fast_nm_suppression.py

--- fast_nm_suppression.py
+++ fast_nm_suppression.py
@@ -1,6 +1,8 @@
 # this is a re-implementation of the http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
 
 import numpy as np
+
+
 class FNMSuppression(object):
     def __init__(self, bbs, overlapThreshold = 0.45):
         self.bbs = bbs
