7f68766c5692eae5712c060c750e13c214eace43
Iryna <ivanovska.iryna@gmail.com>
2017-03-22 20:06:28 +0100

    Update fast_nm_suppression.py
    added slow_suppress

--- fast_nm_suppression.py
+++ fast_nm_suppression.py
@@ -1,9 +1,9 @@
-# this is a re-implementation of the http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
+# http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
 
 import numpy as np
 
 
-class FNMSuppression(object):
+class NMSuppression(object):
     def __init__(self, bbs, overlapThreshold = 0.45):
         self.bbs = bbs
         self.overlapThreshold = overlapThreshold
@@ -53,7 +53,66 @@ class FNMSuppression(object):
 
         return overlap
 
-    def suppress(self):
+    def slow_suppress(self):
+        self.check_empty()
+        self.check_dtype()
+
+        # initialize the list of picked indexes
+        picked = []
+
+        x1, y1, x2, y2 = self.bb_coordinates()
+
+        # compute the area of the bounding boxes
+        area = self.bb_area()
+
+        # sort the bounding boxes by the bottom-right y-coordinate of the bounding box
+        idxs = np.argsort(y2)
+
+        # keep looping while some indexes still remain in the indexes list
+        while len(idxs) > 0:
+            # grab the last index in the indexes list, add the index
+            # value to the list of picked indexes, then initialize
+            # the suppression list (i.e. indexes that will be deleted)
+            # using the last index
+            last = len(idxs) - 1
+            i = idxs[last]
+            picked.append(i)
+            suppress = [last]
+
+            # loop over all indexes in the indexes list
+            for pos in xrange(0, last):
+                # grab the current index
+                j = idxs[pos]
+
+                # find the largest (x, y) coordinates for the start of
+                # the bounding box and the smallest (x, y) coordinates
+                # for the end of the bounding box
+                xx1 = max(x1[i], x1[j])
+                yy1 = max(y1[i], y1[j])
+                xx2 = min(x2[i], x2[j])
+                yy2 = min(y2[i], y2[j])
+
+                # compute the width and height of the bounding box
+                w = max(0, xx2 - xx1 + 1)
+                h = max(0, yy2 - yy1 + 1)
+
+                # compute the ratio of overlap between the computed
+                # bounding box and the bounding box in the area list
+                overlap = float(w * h) / area[j]
+
+                # if there is sufficient overlap, suppress the
+                # current bounding box
+                if overlap > self.overlapThreshold:
+                    suppress.append(pos)
+
+            # delete all indexes from the index list that are in the
+            # suppression list
+            idxs = np.delete(idxs, suppress)
+
+        # return only the bounding boxes that were picked
+        return bbs[picked]
+
+    def fast_suppress(self):
         self.check_empty()
         self.check_dtype()
 
